3a0eeff80d447ea4a46649f055127c490d1946fe
jjayaraaj <jjayaraaj@gmail.com>
2025-07-09 19:37:20 +0530

    Clerk: Auth is temp disabled

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,8 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/api(.*)",
+  "/dashboard(.*)",
+  "/itinerary(.*)"
 ]);
 
 export default clerkMiddleware(async (auth, request) => {
